cb6730e055725a6b9c1da65eb40eca95e39ce2a1
Juifdev20 <maestrodieudonne964@gmail.com>
2026-04-08 07:31:41 +0200

    Backup le 08 avril night create data base fix urle

--- hospital-backend/src/main/java/com/hospital/backend/controller/PatientController.java
+++ hospital-backend/src/main/java/com/hospital/backend/controller/PatientController.java
@@ -19,6 +19,7 @@ import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
 import org.springframework.security.core.Authentication;
+import java.security.Principal;
 import java.util.List;
 
 @RestController
