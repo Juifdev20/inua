059271e5b129905f54bd010906b1722c253a2d05
Juifdev20 <maestrodieudonne964@gmail.com>
2026-04-08 08:28:02 +0200

    deplicate deleted

--- hospi-frontend/src/src/types/finance.js
+++
@@ -1,9 +0,0 @@
-// src/types/finance.js
-export const ExpenseCategory = {
-  SALARY: "SALARY",
-  EQUIPMENT: "EQUIPMENT",
-  MAINTENANCE: "MAINTENANCE",
-  SUPPLIES: "SUPPLIES",
-  UTILITIES: "UTILITIES",
-  OTHER: "OTHER"
-};
